d068f172cd1942902f35c62e5f312ad93600c906
Aweroro Hadi <hadistic20@gmail.com>
2025-01-03 16:39:45 +0100

    refactor(landing-page): added responsiveness to landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,23 +7,21 @@ import Image from 'next/image';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className='lg:fixed w-full'>
+    <html lang="en" className="h-full">
+      <body className="h-full">
         <UserProvider>
-          <div className="relative min-h-screen overflow-hidden">
-            <div className="absolute inset-0 -z-10">
-              <Image
-                src="/assets/bg-image6.jpg"
-                alt="Background"
-                className="w-full h-full"
-                fill
-              />
-            </div>
-          <main id="app" className="flex flex-col min-h-screen">
-            <NavBar />
-              <div className="flex-grow mt-24">{children}</div>
-          </main>
+          <div className="fixed inset-0 -z-10">
+            <Image
+              src="/assets/bg-image6.jpg"
+              alt="Background"
+              className="object-cover w-full h-full"
+              fill
+            />
           </div>
+          <main id="app" className="relative flex flex-col min-h-screen">
+            <NavBar />
+            <div className="flex-grow mt-24">{children}</div>
+          </main>
         </UserProvider>
       </body>
     </html>
